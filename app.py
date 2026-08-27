e6538e6e342c24d78ccc09d2609f66f330f327d7
Gsr1989 <empresalorens@gmail.com>
2026-08-27 12:38:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -472,7 +472,7 @@ def generar_pdf(datos: dict) -> str:
             pg.insert_text((148, 295), marca,         fontsize=S,  fontname=F,  color=(0,0,0))
             # Linea / Tipo
             pg.insert_text((148, 315), linea,         fontsize=S,  fontname=F,  color=(0,0,0))
-            pg.insert_text((320, 315), tipo,          fontsize=S,  fontname=F,  color=(0,0,0))
+            pg.insert_text((330, 315), tipo,          fontsize=S,  fontname=F,  color=(0,0,0))
             # Clase / Modelo
             pg.insert_text((148, 336), clase,         fontsize=S,  fontname=F,  color=(0,0,0))
             pg.insert_text((320, 336), modelo,        fontsize=S,  fontname=F,  color=(0,0,0))
